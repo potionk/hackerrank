bad7a358265bfff02be0e720594f073476590074
potionk <potionkr@gmail.com>
2020-11-01 19:23:08 +0900

    Add Day 1: Data Types solution

--- src/thirtydaysofcode/day1/Solution.java
+++ src/thirtydaysofcode/day1/Solution.java
@@ -3,21 +3,32 @@ package thirtydaysofcode.day1;
 import java.util.Scanner;
 
 public class Solution {
+
     public static void main(String[] args) {
-        // Create a Scanner object to read input from stdin.
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
         Scanner scan = new Scanner(System.in);
 
-        // Read a full line of input from stdin and save it to our variable, inputString.
-        String inputString = scan.nextLine();
+        /* Declare second integer, double, and String variables. */
 
-        // Close the scanner object, because we've finished reading
-        // all of the input from stdin needed for this challenge.
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+scan.nextInt());
+        /* Print the sum of the double variables on a new line. */
+        System.out.println(d+scan.nextDouble());
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+        StringBuilder sb=new StringBuilder();
+        sb.append(s);
+        while(scan.hasNext()){
+            sb.append(scan.next());
+            sb.append(" ");
+        }
+        System.out.println(sb.toString());
         scan.close();
-
-        // Print a string literal saying "Hello, World." to stdout.
-        System.out.println("Hello, World.");
-
-        // TODO: Write a line of code here that prints the contents of inputString to stdout.
-        System.out.println(inputString);
     }
 }
