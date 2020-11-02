2a38a7bc4df9cef95b9ba4a51907d45aa14a9d2f
potionk <potionkr@gmail.com>
2020-11-02 19:49:35 +0900

    Add Day 3: Intro to Conditional Statements solution

---
+++ src/thirtydaysofcode/day3/Solution.java
@@ -0,0 +1,21 @@
+package thirtydaysofcode.day3;
+
+import java.util.Scanner;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N%2==1){
+            System.out.println("Weird");
+        } else {
+            if(6<=N&&N<=20){
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
+        }
+        scanner.close();
+    }
+}
